5de170732a732b043cf582a7e88040f0f2cde7c0
Shawn Presser <shawnpresser@gmail.com>
2022-03-08 20:31:34 -0600

    feat: Initial custom backend

--- src/jax_backend/__init__.py
+++ src/jax_backend/__init__.py
@@ -1,4 +1,128 @@
+from __future__ import annotations
 # read version from installed package
 from importlib.metadata import version
 __version__ = version(__name__)
 del version
+
+from typing import Any, List, Dict, Sequence, Optional
+import warnings
+import os
+
+from absl import logging
+# Disable "WARNING: Logging before flag parsing goes to stderr." message
+logging._warn_preinit_stderr = 0
+
+# import tsp.compiler
+# from tsp import runtime as tsp_runtime
+from backport.dataclasses import dataclass, field, InitVar, KW_ONLY, MISSING
+import dataclasses
+
+from jax._src.lib import xla_client
+from jax._src.lib import xla_bridge
+import numpy as np
+
+import jax
+import jax.numpy as jnp
+import abc
+
+class CustomBackendBuffer(abc.ABC):
+  def __init__(self, device: CustomBackendDevice):
+    super().__init__()
+    self._device = device
+
+  def device(self) -> CustomBackendDevice:
+    return self._device
+
+  @abc.abstractmethod
+  def to_py(self) -> np.ndarray:
+    raise NotImplementedError
+
+  def platform(self):
+    return self.device().client.platform
+
+  def copy_to_device(self, device: CustomBackendDevice) -> CustomBackendBuffer:
+    raise NotImplementedError
+
+
+class CustomBackendExecutable(abc.ABC):
+  def __init__(self, client: CustomBackendClient):
+    super().__init__()
+    self.client = client
+
+  def local_devices(self) -> List[CustomBackendDevice]:
+    return self.client.local_devices()[0:1]
+
+  @abc.abstractmethod
+  def execute(self, arguments: Sequence[CustomBackendBuffer]) -> List[CustomBackendBuffer]:
+    raise NotImplementedError
+
+
+class CustomBackendDevice(abc.ABC):
+  def __init__(self, client: CustomBackendClient, id: int):
+    super().__init__()
+    self.client = client
+    self.id = id
+
+  @property
+  def process_index(self):
+    return 0
+
+class CustomBackendClient(abc.ABC):
+  def __init__(self, devices: List[CustomBackendDevice], local_devices: List[CustomBackendDevice] = None):
+    super().__init__()
+    self._devices = list(devices)
+    self._local_devices = local_devices if local_devices is not None else list(devices)
+
+  def process_index(self):
+    return 0
+
+  @property
+  def platform(self):
+    return self.name()
+
+  def device_count(self):
+    return len(self.devices())
+
+  def devices(self) -> List[CustomBackendDevice]:
+    return list(self._devices)
+
+  def local_devices(self) -> List[CustomBackendDevice]:
+    return list(self._local_devices)
+
+  @abc.abstractmethod
+  def compile(self, computation: str, compile_options: xla_client.CompileOptions) -> CustomBackendExecutable:
+    raise NotImplementedError
+
+
+  @abc.abstractmethod
+  def buffer_from_pyval(
+      self,
+      argument: Any,
+      device: CustomBackendDevice,
+      force_copy: bool = True,
+      host_buffer_semantics: xla_client.HostBufferSemantics = xla_client.HostBufferSemantics.ZERO_COPY
+  ) -> CustomBackendBuffer:
+    # TODO(phawkins): TSP's python API will accept a numpy array directly but
+    # may want to explicitly construct a lower level BufferView to avoid copies.
+    # return TspBuffer(self, device, np.array(argument, copy=True))
+    raise NotImplementedError
+
+
+  @classmethod
+  @abc.abstractmethod
+  def name(cls):
+    raise NotImplementedError
+
+  @classmethod
+  def factory(cls):
+    return cls()
+
+  @classmethod
+  def register(cls):
+    xla_bridge.register_backend_factory(cls.name(), cls.factory, priority=-100)
+
+
+# def custom_backend_client_factory():
+#   return CustomBackendClient()
+#
+# xla_bridge.register_backend_factory("custom", custom_backend_client_factory, priority=-100)
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,78 @@
+from __future__ import annotations
 import unittest
 
-import jax_backend
+import os
 
-class TestCase(unittest.TestCase):
+
+os.environ['JAX_PLATFORM_NAME'] = 'mine'
+
+from absl.testing import absltest
+from jax._src import test_util as jtu
+
+from jax._src.lib import xla_client
+from jax._src.lib import xla_bridge
+
+import jax
+from jax import numpy as jnp
+import numpy as np
+
+from jax.config import config
+config.parse_flags_with_absl()
+
+import jax.interpreters.mlir as mlir
+from jax._src.lib.mlir import ir
+
+import jax_backend
+from typing import List, Any, Sequence
+
+class MyBackendBuffer(jax_backend.CustomBackendBuffer):
+  def __init__(self, device: jax_backend.CustomBackendDevice, value):
+    super().__init__(device)
+    self.value = value
+
+  def to_py(self) -> np.ndarray:
+    return self.value
+
+  def copy_to_device(self, device: jax_backend.CustomBackendDevice) -> jax_backend.CustomBackendBuffer:
+    return MyBackendBuffer(device, self.value)
+
+class MyBackendExecutable(jax_backend.CustomBackendExecutable):
+  def execute(self, arguments: Sequence[MyBackendBuffer]) -> List[MyBackendBuffer]:
+    return [arguments[0]]
+
+class MyBackendDevice(jax_backend.CustomBackendDevice):
+  pass
+
+class MyBackendClient(jax_backend.CustomBackendClient):
+  def __init__(self):
+    devices = [MyBackendDevice(self, id=i) for i in range(8)]
+    super().__init__(devices)
+
+  @classmethod
+  def name(cls):
+    return "mine"
+
+  def buffer_from_pyval(
+      self,
+      argument: Any,
+      device: jax_backend.CustomBackendDevice,
+      force_copy: bool = True,
+      host_buffer_semantics: xla_client.HostBufferSemantics = xla_client.HostBufferSemantics.ZERO_COPY
+  ) -> jax_backend.CustomBackendBuffer:
+    return MyBackendBuffer(device, np.array(argument, copy=True))
+
+  def compile(self, computation: str, compile_options: xla_client.CompileOptions) -> jax_backend.CustomBackendExecutable:
+    return MyBackendExecutable(self)
+
+MyBackendClient.register()
+
+class TestCase(jtu.JaxTestCase):
   def test_basic(self):
     self.assertEqual(1, 1)
+    self.assertEqual("mine", xla_bridge.get_backend().platform)
+    self.assertEqual(8, len(jax.devices()))
+    one = jax.device_put(1, jax.devices()[0])
+    three = one + 2
 
 if __name__ == '__main__':
-  unittest.main()
+  absltest.main(testLoader=jtu.JaxTestLoader())
